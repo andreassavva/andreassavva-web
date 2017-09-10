722ada8cf9fc7bbe73d38766b34fe66bb7fc5d5c
Andreas Savva <ens15asa@cs.umu.se>
2017-09-10 20:32:56 +0200

    react-create-app on Heroku

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Andreas Savva, Front-End Developer</h2>
+        </div>
+        <pre className="App-intro">
+          Contact me: andreas.k.savva@gmail.com
+        </pre>
+      </div>
+    );
+  }
+}
+
+export default App;
